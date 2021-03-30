6de083b790d0d8d20f5c7550ed75792732fc4e2b
a-roseanne <angelica_roseanne@yahoo.co.id>
2021-03-30 14:58:22 +0700

    cobaaaaaa

--- trialSemut/GameScene.swift
+++ trialSemut/GameScene.swift
@@ -28,6 +28,7 @@ class GameScene: SKScene {
             label.run(SKAction.fadeIn(withDuration: 2.0))
         }
         
+        //cobaaaaaaaa
         // Create shape node to use during mouse interaction
         let w = (self.size.width + self.size.height) * 0.05
         self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
